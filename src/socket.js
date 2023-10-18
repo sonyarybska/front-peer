dedf7648f030c1d5e17622043c7934dd5117aed7
Sofiia <sofaribska@gmail.com>
2023-10-19 02:42:35 +0300

    soc

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-const sockets = io('http://localhost:3001', { autoConnect: true, forceNew: true });
+const sockets = io('https://test-peer-backfv.onrender.com/', { autoConnect: true, forceNew: true });
 
 export default sockets;
